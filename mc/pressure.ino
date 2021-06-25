5a81e4f39e8b461f3a0455c1c258d1f681937c2d
Nihal Kumarswamy <kumarnihal04@gmail.com>
2021-06-24 20:52:35 -0500

    added some rough pressure code for mc

--- mc/pressure.ino
+++ mc/pressure.ino
@@ -1,13 +1,52 @@
+const int sens_1 = PA0;
+const int sens_2 = PA1;
+const int sens_3 = PA2;
+const int sens_4 = PA3;
+const int sens_5 = PA4;
+const int sens_6 = PA5;
+const int sens_7 = PA6;
+const int sens_8 = PA7;
+const int sens_9 = PB0;
+const int sens_10 = PB1;
+
+const unsigned long BAUD_RATE = 9600;
+
 void setup(){
+    pinMode(sens_1, INPUT_ANALOG);
+    pinMode(sens_2, INPUT_ANALOG);
+    pinMode(sens_3, INPUT_ANALOG);
+    pinMode(sens_4, INPUT_ANALOG);
+    pinMode(sens_5, INPUT_ANALOG);
+    pinMode(sens_6, INPUT_ANALOG);
+    pinMode(sens_7, INPUT_ANALOG);
+    pinMode(sens_8, INPUT_ANALOG);
+    pinMode(sens_9, INPUT_ANALOG);
+    pinMode(sens_10, INPUT_ANALOG);
 
-
+    Serial.begin(9600);
 
 }
 
 
 void loop(){
 
+    int sens_1_val, sens_2_val, sens_3_val, sens_4_val, sens_5_val,
+        sens_6_val, sens_7_val, sens_8_val, sens_9_val, sens_10_val;
+    
+    sens_1_val = analogRead(sens_1);
+    sens_2_val = analogRead(sens_2);
+    sens_3_val = analogRead(sens_3);
+    sens_4_val = analogRead(sens_4);
+    sens_5_val = analogRead(sens_5);
+    sens_6_val = analogRead(sens_6);
+    sens_7_val = analogRead(sens_7);
+    sens_8_val = analogRead(sens_8);
+    sens_9_val = analogRead(sens_9);
+    sens_10_val = analogRead(sens_10);
 
-
-
+    Serial.println(sens_1_val+', '+sens_2_val+', '+sens_3_val+', '+sens_4_val+
+                    ', '+sens_5_val+', '+sens_6_val+', '+sens_7_val+', '+sens_8_val+', '+
+                    sens_9_val+', '+sens_10_val);
+    
+    delay(5);
 }
--- mc/valve.ino
+++ mc/valve.ino
@@ -12,76 +12,86 @@ const int valve4 = PB6;
 const int valve5 = PB5;
 const int valve6 = PB4;
 
-void setup() {
-  // put your setup code here, to run once:
-pinMode(valve1,OUTPUT);
-pinMode(valve2,OUTPUT);
-pinMode(valve3,OUTPUT);
-pinMode(valve4,OUTPUT);
-pinMode(valve5,OUTPUT);
-pinMode(valve6,OUTPUT);
+const unsigned long BAUD_RATE = 9600;
 
-digitalWrite(valve1, LOW);
-digitalWrite(valve2, LOW);
-digitalWrite(valve1, LOW);
-digitalWrite(valve2, LOW);
-digitalWrite(valve1, LOW);
-digitalWrite(valve2, LOW);
+void setup()
+{
+   // put your setup code here, to run once:
+   pinMode(valve1, OUTPUT);
+   pinMode(valve2, OUTPUT);
+   pinMode(valve3, OUTPUT);
+   pinMode(valve4, OUTPUT);
+   pinMode(valve5, OUTPUT);
+   pinMode(valve6, OUTPUT);
 
-Serial.begin(9600);
+   digitalWrite(valve1, LOW);
+   digitalWrite(valve2, LOW);
+   digitalWrite(valve1, LOW);
+   digitalWrite(valve2, LOW);
+   digitalWrite(valve1, LOW);
+   digitalWrite(valve2, LOW);
+
+   Serial.begin(BAUD_RATE);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  int incoming;
-if(Serial.available() >0){
-  incoming = Serial.read();
-  if (incoming == 65 ){
-      valve1state = (!valve1state);
-      if (valve1state == true)
-      digitalWrite(valve1, HIGH);
-      else
-      digitalWrite(valve1, LOW);
-  }
-  else  if (incoming == 66 ){
-      valve2state =! valve2state;
-      if (valve2state == true)
-      digitalWrite(valve2, HIGH);
-      else 
-      digitalWrite(valve2, LOW);
-  }
-  
-   else  if (incoming == 67 ){
-      valve3state =! valve3state;
-      if (valve3state == true)
-      digitalWrite(valve3, HIGH);
-      else 
-      digitalWrite(valve3, LOW);
-   }  
-  
-   else  if (incoming == 68 ){
-      valve4state =! valve4state;
-      if (valve4state == true)
-      digitalWrite(valve4, HIGH);
-      else 
-      digitalWrite(valve4, LOW);
-   }  
-     else  if (incoming == 69 ){
-      valve5state =! valve5state;
-      if (valve5state == true)
-      digitalWrite(valve5, HIGH);
-      else 
-      digitalWrite(valve5, LOW);
+void loop()
+{
+   // put your main code here, to run repeatedly:
+   int incoming;
+   if (Serial.available() > 0)
+   {
+      incoming = Serial.read();
+      if (incoming == 65)
+      {
+         valve1state = (!valve1state);
+         if (valve1state == true)
+            digitalWrite(valve1, HIGH);
+         else
+            digitalWrite(valve1, LOW);
+      }
+      else if (incoming == 66)
+      {
+         valve2state = !valve2state;
+         if (valve2state == true)
+            digitalWrite(valve2, HIGH);
+         else
+            digitalWrite(valve2, LOW);
+      }
+
+      else if (incoming == 67)
+      {
+         valve3state = !valve3state;
+         if (valve3state == true)
+            digitalWrite(valve3, HIGH);
+         else
+            digitalWrite(valve3, LOW);
+      }
+
+      else if (incoming == 68)
+      {
+         valve4state = !valve4state;
+         if (valve4state == true)
+            digitalWrite(valve4, HIGH);
+         else
+            digitalWrite(valve4, LOW);
+      }
+      else if (incoming == 69)
+      {
+         valve5state = !valve5state;
+         if (valve5state == true)
+            digitalWrite(valve5, HIGH);
+         else
+            digitalWrite(valve5, LOW);
+      }
+      else if (incoming == 70)
+      {
+         valve6state = !valve6state;
+         if (valve6state == true)
+            digitalWrite(valve6, HIGH);
+         else
+            digitalWrite(valve6, LOW);
+      }
+
+      delay(100);
    }
-      else  if (incoming == 70 ){
-      valve6state =! valve6state;
-      if (valve6state == true)
-      digitalWrite(valve6, HIGH);
-      else 
-      digitalWrite(valve6, LOW);
-   }  
-    
-  
-delay(100);
-}
 }
