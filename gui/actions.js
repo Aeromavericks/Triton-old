b9d2b9623e2637c1e3abba4f31a43c09cff46113
Thevnin <86437442+Thevnin@users.noreply.github.com>
2021-07-08 17:49:57 -0500

    Gauges
    Here are the Gauges

--- gui/actions.js
+++ gui/actions.js
@@ -38,4 +38,52 @@ function valve(ele){
 
     
   }
+  
+/* */
+$(document).ready(function(){
+  
+  var mc = {
+    '0-19'     : 'red',
+    '20-59'    : 'orange',
+    '60-100'   : 'green'
+  };
+  
+function between(x, min, max) {
+  return x >= min && x <= max;
+}
+  
+
+  
+  var dc;
+  var first; 
+  var second;
+  var th;
+  
+  $('p').each(function(index){
+    
+    th = $(this);
+    
+    dc = parseInt($(this).attr('data-color'),10);
+    
+    
+      $.each(mc, function(name, value){
+        
+        
+        first = parseInt(name.split('-')[0],10);
+        second = parseInt(name.split('-')[1],10);
+        
+        console.log(between(dc, first, second));
+        
+        if( between(dc, first, second) ){
+          th.addClass(value);
+        }
+
+    
+    
+      });
+    
+  });
+});
+
+/* gauges*/
 
